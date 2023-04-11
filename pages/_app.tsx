e9bae3582a747f584ffe7112dd8ee9ea7df5f60d
t-riku <dingariku@icloud.com>
2023-04-11 19:19:30 +0900

    ✨ ApolloProviderを初期化

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const GITHUB_BASE_URL = "https://api.github.com/graphql";
+
+function App({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: GITHUB_BASE_URL,
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_APP_GITHUB_ACCESS_TOKEN}`,
+    },
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
+
+export default App;
